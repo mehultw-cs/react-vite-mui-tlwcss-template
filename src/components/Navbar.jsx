17664ce39cf9c50e2cdd3ba868817a7ad28ba86b
Mehul Tanwar <mehultw.cs@gmail.com>
2022-07-31 17:15:45 -0300

    updated header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
     <div className="fixed left-0 right-0 top-0 h-16 shadow-md border-b-2 border-gray-100 bg-gray-900">
       <nav className="flex items-center container mx-auto h-full justify-between">
         <h1 className="font-semibold uppercase text-lg text-gray-200">
-          🔄 Demo App
+           💫 Vite React App
         </h1>
         <div>
           <ul className="flex items-center space-x-10 text-sm">
